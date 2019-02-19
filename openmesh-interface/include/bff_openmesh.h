a35673cf4dd00d1325149a641f2d820e9713fc14
Alexander Schier <schier@uni-bonn.de>
2019-02-19 14:59:48 +0100

    Added an OpenMesh interface
    Added an interface to parameterize OpenMesh triangle meshes.
    New dependency:
    openmesh_structures (https://github.com/aschier/openmesh_structures)

---
+++ openmesh-interface/include/bff_openmesh.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <Mesh.h>
+#include <OpenMesh/Core/Mesh/TriMesh_ArrayKernelT.hh>
+#include <openmesh_doubleprecisiontraits.h>
+
+namespace bff {
+	typedef OpenMesh::TriMesh_ArrayKernelT<OpenMesh::DoublePrecisionTraits> TriMesh;
+	bff::Mesh openmesh_to_bff_mesh(const TriMesh &mesh);
+	TriMesh bff_mesh_to_openmesh(bff::Mesh &mesh);
+}
